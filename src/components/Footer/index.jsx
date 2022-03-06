3badad9fa2d9745a0850a55b4d96364267ad7df0
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-03-06 18:57:23 +0100

    Adding links to the footer content

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,9 +6,9 @@ function Footer() {
         <div className='flex-col'>
             <div className='font-extrabold text-7xl opacity-70'>Contact</div> 
             <div className='py-4'>
-                <a className="pr-3 opacity-30 hover:opacity-70" href="#about">Email</a>
-                <a className="pr-3 opacity-30 hover:opacity-70" href="#blogs">Linkedin</a>
-                <a className="opacity-30 hover:opacity-70" href="#blogs">Github</a>
+                <a className="pr-3 opacity-30 hover:opacity-70" href="mailto:jrodriguezo.developer@gmail.com">Email</a>
+                <a className="pr-3 opacity-30 hover:opacity-70" href="https://es.linkedin.com/in/jes%C3%BAs-rodr%C3%ADguez-ovejero-b704131b4?trk=profile-badge" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                <a className="opacity-30 hover:opacity-70" href="https://github.com/jrodriguezo" target="_blank" rel="noopener noreferrer">Github</a>
             </div>
         </div>
     </footer>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,10 @@ export default function Header() {
     return ( 
       <header>
         <nav className="flex flex-wrap pt-5 items-center max-w-5xl text-3xl mx-auto justify-between">
-            <Link className="link" to="/">jrodriguezo.dev</Link>
+            <Link className="link" to="/">&lt;jrodriguezo &frasl;&gt;</Link>
             <div>
                   <a className="mr-8" href="#about">About</a>
-                  <a className="mr-8" href="#blogs">Blogs</a>
-                  <Link className="mr-8" to="/blog/fitness">Projects</Link>
-                  <Link to="/blog/fitness">Blogs</Link>
+                  <a href="#blogs">Blogs</a>
             </div>
         </nav>
       </header>
