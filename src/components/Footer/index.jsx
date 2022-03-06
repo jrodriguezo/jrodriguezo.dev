a6530bb2c14a3bce65a02af3d8f908ff05e3771e
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-03-06 22:01:09 +0100

    Dot middle separation added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,8 +6,10 @@ function Footer() {
         <div className='flex-col'>
             <div className='font-extrabold text-7xl opacity-70'>Contact</div> 
             <div className='py-4'>
-                <a className="pr-3 opacity-30 hover:opacity-70" href="mailto:jrodriguezo.developer@gmail.com">Email</a>
-                <a className="pr-3 opacity-30 hover:opacity-70" href="https://es.linkedin.com/in/jes%C3%BAs-rodr%C3%ADguez-ovejero-b704131b4?trk=profile-badge" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                <a className="opacity-30 hover:opacity-70" href="mailto:jrodriguezo.developer@gmail.com">Email</a>
+                <span className='px-2 opacity-30'>·</span>
+                <a className="opacity-30 hover:opacity-70" href="https://es.linkedin.com/in/jes%C3%BAs-rodr%C3%ADguez-ovejero-b704131b4?trk=profile-badge" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                <span className='px-2 opacity-30'>·</span>
                 <a className="opacity-30 hover:opacity-70" href="https://github.com/jrodriguezo" target="_blank" rel="noopener noreferrer">Github</a>
             </div>
         </div>
