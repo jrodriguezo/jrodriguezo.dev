3bfb48fc2d34b311bc6b7e81099d96e3f7af9062
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-03-27 18:04:19 +0200

    feat: UnderConstruction component done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,6 +6,7 @@ import Philosophy from "../Philosophy";
 import BlockWithTitleTextAndImageReversed from "../../components/BlockWithTitleTextAndImageReversed";
 
 import "./styles.css";
+import UnderConstruction from "../../components/UnderConstruction";
 
 export default function Home() {
   return (
@@ -19,14 +20,7 @@ export default function Home() {
           <BlockWithTitleTextAndImage />
           <Philosophy />
           <BlockWithTitleTextAndImageReversed />
-          <div className="bg-darkness text-yellow-rick-morty h-5/6 my-10 py-40  overflow-hidden">
-            <div className="flex flex-col mx-auto p-10 sm:w-1/2 font-bold text-4xl sm:text-7xl text-center sm:px-4 sm:my-10">
-              <p>This website is currently under construction.</p>
-              <p className="text-xl text-zinc-300 opacity-30"> Last update was on March 26th, 2022</p>
-              <p className="mt-20">Visit: </p>
-              <a className="hover:opacity-60 text-3xl text-white" href="https://jrodriguezo.github.io/">jrodriguezo.github.io</a>
-            </div>
-          </div>
+          <UnderConstruction />
         </div>
       </section>
     </div>
