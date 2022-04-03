b8ba8fc45dad77553c11b7f1feb66a2c564a05cd
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-04-03 23:02:09 +0200

    add: link to project

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -9,31 +9,35 @@ function Projects() {
             Projects
           </p>
           <div className="text-xl text-center mt-2 opacity-90">
-            made in my free time or university
+            made in my free time
           </div>
-          <div class="relative wrap overflow-hidden p-10 h-full">
-            <div class="border-2-2 absolute border-opacity-20 border-brown-rick-morty h-full border left-1/2"></div>
+          <div className="relative wrap overflow-hidden p-10 h-full">
+            <div className="border-2-2 absolute border-opacity-20 border-brown-rick-morty h-full border left-1/2"></div>
 
-            <div class="mb-8 flex justify-between items-center w-full right-timeline">
-              <div class="order-1 w-5/12"></div>
-              <div class="z-20 flex items-center order-1 bg-brown-rick-morty shadow-xl w-8 h-8 rounded-full">
-                <h1 class="mx-auto font-semibold text-lg text-white">1</h1>
+            <div className="mb-8 flex justify-between items-center w-full right-timeline">
+              <div className="order-1 w-5/12"></div>
+              <div className="z-20 flex items-center order-1 bg-brown-rick-morty shadow-xl w-8 h-8 rounded-full">
+                <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
               </div>
-              <div class="order-1 bg-brown-rick-morty rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-3 font-bold text-gray-800 text-xl">
+              <div className="order-1 bg-brown-rick-morty rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 className="mb-3 font-bold text-gray-800 text-xl">
                   eSignature & blockchain
                 </h3>
-                <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
                   Web application for electronic signature mechanisms using the
                   building block CEF eSignature that complying with the eIDAS
                   regulation, the validation with ETSI and the Self-Sovereign
                   digital Identity management via Blockchain on the Ethereum
                   network using Smart Contracts.
                 </p>
-                <h4 class="mb-3 font-bold text-gray-800 text-lg mt-10">
+                <a href="https://google.es" type="button" class="text-white bg-brawn-rick-morty opacity-50 hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 my-4">
+                  <svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
+                  See code <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+                <h4 className="mb-3 font-bold text-gray-800 text-lg mt-4">
                   Main Technologies used
                 </h4>
-                <ul>
+                <ul className>
                   <li className="inline-block rounded-full text-white 
                     bg-black hover:bg-slate-500 duration-1000 
                     text-xs font-bold 
@@ -48,18 +52,26 @@ function Projects() {
                     opacity-90 hover:opacity-100">
                             Web3js
                   </li>
+                  <li className="inline-block rounded-full text-white 
+                    bg-black hover:bg-gray-500 duration-300 
+                    text-xs font-bold 
+                    mr-1 md:mr-2 mb-2 px-2 md:px-4 py-1 
+                    opacity-90 hover:opacity-100">
+                            Bootstrap
+                  </li>
                 </ul>
+
               </div>
             </div>
 
-            <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-              <div class="order-1 w-5/12"></div>
-              <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                <h1 class="mx-auto text-white font-semibold text-lg">2</h1>
+            <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+              <div className="order-1 w-5/12"></div>
+              <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
               </div>
-              <div class="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-                <p class="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
+              <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
+                <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
                   standard dummy text ever since the 1500s, when an unknown
@@ -69,16 +81,16 @@ function Projects() {
               </div>
             </div>
 
-            <div class="mb-8 flex justify-between items-center w-full right-timeline">
-              <div class="order-1 w-5/12"></div>
-              <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                <h1 class="mx-auto font-semibold text-lg text-white">3</h1>
+            <div className="mb-8 flex justify-between items-center w-full right-timeline">
+              <div className="order-1 w-5/12"></div>
+              <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                <h1 className="mx-auto font-semibold text-lg text-white">3</h1>
               </div>
-              <div class="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-3 font-bold text-gray-800 text-xl">
+              <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 className="mb-3 font-bold text-gray-800 text-xl">
                   Lorem Ipsum
                 </h3>
-                <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
                   standard dummy text ever since the 1500s, when an unknown
@@ -88,14 +100,14 @@ function Projects() {
               </div>
             </div>
 
-            <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-              <div class="order-1 w-5/12"></div>
-              <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                <h1 class="mx-auto text-white font-semibold text-lg">4</h1>
+            <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+              <div className="order-1 w-5/12"></div>
+              <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                <h1 className="mx-auto text-white font-semibold text-lg">4</h1>
               </div>
-              <div class="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                <h3 class="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-                <p class="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
+              <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
+                <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
                   standard dummy text ever since the 1500s, when an unknown
@@ -105,6 +117,7 @@ function Projects() {
               </div>
             </div>
           </div>
+          <p>Carry on... </p>
         </div>
       </div>
     </div>
