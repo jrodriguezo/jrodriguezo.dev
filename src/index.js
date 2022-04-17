9ae86ea9f13d3093cb96bd5d6a97be9c66c81d80
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-04-17 20:32:12 +0200

    chore: upgrading project to React 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
