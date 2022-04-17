0b6753528e364a380b285b4390c6597a9c6bf8a5
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-04-17 19:31:09 +0200

    fix: padding style added

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -4,12 +4,12 @@ import ErrorGif from "../../components/ErrorGif";
 
 function PageNotFound() {
   return (
-    <div className="flex flex-col mx-auto max-w-3xl items-center min-h-screen">
-      <p className="text-5xl opacity-90 mt-20 text-red-400">PAGE NOT FOUND</p>
-      <p className="text-xl  opacity-30 mb-20 tracking-widest"><span className="px-2">·</span>ERROR 404<span className="px-2">·</span></p>
+    <div className="p-5 flex flex-col mx-auto max-w-3xl sm:items-center min-h-screen">
+      <p className="text-5xl opacity-90 mt-20 mb-10 sm:mb-0 text-red-400">PAGE NOT FOUND</p>
+      <p className="text-xl  opacity-30 mb-20 tracking-widest hidden sm:block"><span className="px-2">·</span>ERROR 404<span className="px-2">·</span></p>
       <div className="absolute top-1/2 left-1/3 mt-20"></div>
       <ErrorGif />
-      <p className="p-5 text-xl opacity-50 mt-10 break-words">
+      <p className="mb-4 text-xl opacity-50 mt-10 break-words">
         Sorry, we can't find that page! Don't worry though, everything is under control.  
       </p>
       <Link
