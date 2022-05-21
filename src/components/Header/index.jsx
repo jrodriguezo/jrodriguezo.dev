65c9467050617e1ae348e1715a5e5ee2b5d9a4fd
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-05-21 13:20:33 +0200

    feat: adding new logo & updating information

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -34,7 +34,7 @@ export default function Header() {
           className="text-xl sm:text-2xl font-semibold hover:opacity-60"
           to="/"
         >
-          &lt;jrodriguezo &frasl;&gt;
+          <img src="personal_logo.png" className="h-16" alt="text about my name and first name" />
         </Link>
         <div className="block sm:hidden">
           {!open && <FaHamburger onClick={handleClick} />}
