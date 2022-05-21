371dd100ca63ad1ffcdde69fda4abf1c21350f96
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-05-21 15:51:00 +0200

    feat: including SEO tags with react-helmet

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,24 +1,40 @@
 import React from "react";
+import Helmet from "react-helmet";
 import { Link } from "react-router-dom";
 import ErrorGif from "../../components/ErrorGif";
 
 function PageNotFound() {
   return (
-    <div className="p-5 flex flex-col mx-auto max-w-3xl sm:items-center min-h-screen">
-      <p className="text-5xl opacity-90 mt-20 mb-10 sm:mb-0 text-red-400">PAGE NOT FOUND</p>
-      <p className="text-xl  opacity-30 mb-20 tracking-widest hidden sm:block"><span className="px-2">·</span>ERROR 404<span className="px-2">·</span></p>
-      <div className="absolute top-1/2 left-1/3 mt-20"></div>
-      <ErrorGif />
-      <p className="mb-4 text-xl opacity-50 mt-10 break-words">
-        Sorry, we can't find that page! Don't worry though, everything is under control.  
-      </p>
-      <Link
-        className="text-darkness bg-red-400 hover:opacity-70 font-bold py-2 px-4 rounded-full text-center mt-5"
-        to="/"
-      >
-        Go home page
-      </Link>
-    </div>
+    <>
+      <Helmet>
+        <title>You have lost, this page doesn't exist</title>
+        <meta
+          name="description"
+          content="Page not found, please come back to home page and continue enjoying."
+        />
+      </Helmet>
+      <div className="p-5 flex flex-col mx-auto max-w-3xl sm:items-center min-h-screen">
+        <p className="text-5xl opacity-90 mt-20 mb-10 sm:mb-0 text-red-400">
+          PAGE NOT FOUND
+        </p>
+        <p className="text-xl  opacity-30 mb-20 tracking-widest hidden sm:block">
+          <span className="px-2">·</span>ERROR 404
+          <span className="px-2">·</span>
+        </p>
+        <div className="absolute top-1/2 left-1/3 mt-20"></div>
+        <ErrorGif />
+        <p className="mb-4 text-xl opacity-50 mt-10 break-words">
+          Sorry, we can't find that page! Don't worry though, everything is
+          under control.
+        </p>
+        <Link
+          className="text-darkness bg-red-400 hover:opacity-70 font-bold py-2 px-4 rounded-full text-center mt-5"
+          to="/"
+        >
+          Go home page
+        </Link>
+      </div>
+    </>
   );
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,10 +8,15 @@ import BlockWithTitleTextAndImageReversed from "../../components/BlockWithTitleT
 import "./styles.css";
 import UnderConstruction from "../../components/UnderConstruction";
 import Projects from "../../components/Projects";
+import Helmet from "react-helmet";
 
 export default function Home() {
   return (
     <div>
+      <Helmet>
+        <title>Learn more about me and also check out my blog and projects | Jesús Rodríguez Ovejero</title>
+        <meta name="description" content="Jesús Rodríguez Ovejero is a Telecommunications Engineer specialized in Computer Science. Currently, he works as a Frontend Developer. Visit his little blog and take a look at his most recent projects." />
+      </Helmet>
       <div className="bg-yellow-js text-black-js">
         <Header />
         <CoverPage />
