cb794dd6e10b04d95a3b613d6fb084376c0ded83
jrodriguezo <jrodriguezo@alumnos.upm.es>
2022-09-24 01:40:48 +0200

    feat: add border to quote

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -43,7 +43,7 @@ function AboutMe () {
               I'm addicted to improve with entusiasm every day.
               If I would define what is the best part of coding is when it's made with love;
               obsessed with clean code, SOLID principles and best practices.</li>
-                <li className='mt-10'>If you got here but you didn't figure out anything, don't worry though. <span className='italic my-4 block text-gray-400'><span className="font-serif font-black text-gray-400 text-xl sm:text-3xl">“</span> My job is about writing text using
+                <li className='mt-10'>If you got here but you didn't figure out anything, don't worry though. <span className='italic border-l-2 my-10 pl-6 border-gray-500 font-extralight block text-gray-300'> My job is about writing text using
               random English words and symbols in fancy colors, and the text is not even properly aligned to the left.</span>
               Maybe you got a slight smile, I hope so. Memes and positive vibes with extra positive ingredient, are a must in my life.
                 </li>
