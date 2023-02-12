13d8bf3f9688f569b384ed60baa2563c6054fc63
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2023-02-12 19:13:29 +0100

    fix: condition to apply styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -45,8 +45,8 @@ export default function Header () {
 
   const Routes = () => <nav className='flex flex-col sm:flex-row gap-8 sm:gap-0'>
     {ROUTES.map(({ name, href }) => {
-      return <a key={href} className={`group transition-all duration-300 ease-in-out sm:text-2xl px-3 py-1 opacity-80 hover:opacity-100 ${isVisible ? 'text-[32px]' : 'text-xl'}`} href={href}>
-        <span className={`pb-3 cursor-pointer bg-left-bottom bg-gradient-to-r ${isVisible ? 'from-white to-white' : 'from-black-js to-black-js'} bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out`}>
+      return <a key={href} className={`group transition-all duration-300 ease-in-out sm:text-2xl px-3 py-1 opacity-80 hover:opacity-100 ${open ? 'text-[32px]' : 'text-xl'}`} href={href}>
+        <span className={`pb-3 cursor-pointer bg-left-bottom bg-gradient-to-r ${open ? 'from-white to-white' : 'from-black-js to-black-js'} bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out`}>
           {name}
         </span>
       </a>
