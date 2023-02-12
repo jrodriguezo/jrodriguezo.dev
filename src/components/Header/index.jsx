b3594079152068cfd55ce35565a46dfb6cdf37f3
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2023-02-12 19:07:35 +0100

    feat: change styles of navigation in mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaHamburger, FaRegWindowClose } from 'react-icons/fa'
+import useWindowSize from 'hooks/useWindowSize'
 
 export default function Header () {
   const [open, setOpen] = useState(false)
+  const [width] = useWindowSize()
 
   const handleClick = () => {
     setOpen((prevState) => !prevState)
@@ -24,24 +26,37 @@ export default function Header () {
     }
   ]
 
+  useEffect(() => {
+    if (width > 640) {
+      setOpen(false)
+    }
+  }, [width])
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = 'hidden'
+      return () => {
+        document.body.style.overflow = 'visible'
+      }
+    }
+  }, [open])
+
   const isVisible = open ? 'block' : 'hidden'
 
-  const routes = (
-    <nav className='flex flex-col sm:flex-row'>
-      {ROUTES.map(({ name, href }) => {
-        return <a key={href} className='group transition-all duration-300 ease-in-out text-xl sm:text-2xl px-3 py-1 opacity-80 hover:opacity-100' href={href}>
-          <span className='pb-3 cursor-pointer bg-left-bottom bg-gradient-to-r from-black-js to-black-js bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out'>
-            {name}
-          </span>
-        </a>
-      })}
-    </nav>
-  )
+  const Routes = () => <nav className='flex flex-col sm:flex-row gap-8 sm:gap-0'>
+    {ROUTES.map(({ name, href }) => {
+      return <a key={href} className={`group transition-all duration-300 ease-in-out sm:text-2xl px-3 py-1 opacity-80 hover:opacity-100 ${isVisible ? 'text-[32px]' : 'text-xl'}`} href={href}>
+        <span className={`pb-3 cursor-pointer bg-left-bottom bg-gradient-to-r ${isVisible ? 'from-white to-white' : 'from-black-js to-black-js'} bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out`}>
+          {name}
+        </span>
+      </a>
+    })}
+  </nav>
 
   return (
-    <header>
+    <header className='w-full relative bg-yellow-rick-morty text-black-js'>
       <nav
-        className={'flex flex-wrap p-5 sm:pt-5 items-center lg:px-56 text-3xl mx-auto justify-between'}
+        className='flex flex-wrap p-5 sm:pt-5 items-center lg:px-56 text-3xl mx-auto justify-between'
       >
         <Link
           className='text-xl sm:text-2xl font-semibold hover:opacity-60'
@@ -50,16 +65,19 @@ export default function Header () {
           <img src='personal_logo.png' className='h-10' alt='text about my name and first name' />
         </Link>
         <div className='block sm:hidden transition-opacity easy-in duration-200 hover:opacity-60 cursor-pointer'>
-          {open ? <FaRegWindowClose onClick={handleClick} /> : <FaHamburger onClick={handleClick} />}
+          {!open && <FaHamburger onClick={handleClick} />}
         </div>
         <div className={'hidden flex-col sm:block sm:flex-row items-center'}>
-          {routes}
+          <Routes />
         </div>
       </nav>
       <div
-        className={`flex flex-col ${isVisible} items-end pr-2 mr-8 border-r-2 border-r-black sm:hidden sm:flex-row`}
+        className={`flex flex-col w-full text-center ${isVisible} fixed pr-2 mr-8 border-r-2 border-r-black sm:hidden sm:flex-row min-h-screen inset-0 backdrop-blur-sm z-[1] h-full bg-black opacity-80 justify-center items-center text-white text-2xl`}
       >
-        {routes}
+        <Routes />
+        <button onClick={handleClick}>
+          <FaRegWindowClose className='mt-20 text-[42px] hover:opacity-60' />
+        </button>
       </div>
     </header>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -15,30 +15,33 @@ import Slider from 'components/Slider/slider'
 
 export default function Home () {
   return (
-    <div>
-      <Helmet>
-        <title>
+    <>
+      <Header />
+      <div>
+        <Helmet>
+          <title>
           Learn more about me and also check out my blog and projects | Jesús
           Rodríguez Ovejero
-        </title>
-        <meta
-          name='description'
-          content='Jesús Rodríguez Ovejero is a Telecommunications Engineer specialized in Computer Science. Currently, he works as a Frontend Developer. Visit his little blog and take a look at his most recent projects.'
-        />
-      </Helmet>
-      <div className='bg-yellow-rick-morty text-black'>
-        <Header />
-        <CoverPage />
+          </title>
+          <meta
+            name='description'
+            content='Jesús Rodríguez Ovejero is a Telecommunications Engineer specialized in Computer Science. Currently, he works as a Frontend Developer. Visit his little blog and take a look at his most recent projects.'
+          />
+        </Helmet>
+        <div className='bg-yellow-rick-morty text-black'>
+          <CoverPage />
+        </div>
+        <section className='flex flex-col sm:py-16 py-8 sm:gap-16 gap-8 lg:px-56 text-gray-300 px-3 sm:px-6'>
+          <AboutMe />
+          <Slider />
+          <Skills />
+          <Philosophy />
+          <Blogs />
+          {/* <Projects /> -- TODO: improve how it should be visible */}
+          {/* <UnderConstruction /> -- Release v1.0 */}
+        </section>
       </div>
-      <section className='flex flex-col sm:py-16 py-8 sm:gap-16 gap-8 lg:px-56 text-gray-300 px-3 sm:px-6'>
-        <AboutMe />
-        <Slider />
-        <Skills />
-        <Philosophy />
-        <Blogs />
-        {/* <Projects /> -- TODO: improve how it should be visible */}
-        {/* <UnderConstruction /> -- Release v1.0 */}
-      </section>
-    </div>
+    </>
+
   )
 }
