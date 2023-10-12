dc609b266be91d1081425cb4273455d6c704d95f
jrodriguezo <jrodriguezo@alumnos.upm.es>
2023-10-12 20:14:45 +0200

    feat: add provisional rocket + smoke

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useCallback, useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image";
 
 export default function Home() {
   const [scrollY, setScrollY] = useState(0);
@@ -36,6 +37,23 @@ export default function Home() {
         <h1>Frontend Developer</h1>
         <h3>Jesus Rodriguez Ovejero</h3>
       </div>
+      <Image
+        className={styles.rocket}
+        style={{
+          bottom: (scrollY + 70) * (scrollY > 0 ? 1.2 : 1),
+        }}
+        src={"/assets/images/rocket.png"}
+        alt="rocket"
+        height={560}
+        width={320}
+      />
+      <div
+        className={styles.smoke}
+        style={{
+          visibility: scrollY === 0 ? "hidden" : "visible",
+          bottom: scrollY * (scrollY > 0 ? 1.2 : 1),
+        }}
+      />
       <div className={styles.wrapper}>
         <div className={`${styles.navbar} ${entireStyles}`}>
           <Navbar />
